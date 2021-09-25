6db2746d5e2a74480b508d6259e72d0dfe8f3e8b
doorbash <milad.doorbash@gmail.com>
2021-09-25 15:51:50 +0330

    add production

--- run.sh
+++ run.sh
@@ -15,6 +15,14 @@ if [ "$1" == "clean" ]; then
     exit 0
 fi
 
+chmod 777 docker/pg-admin/
+chmod 777 docker/pg-admin/servers.json
+
+if [ "$1" == "production" ]; then
+    sudo docker-compose pull && sudo docker-compose up -d --force-recreate --no-build
+    exit 0
+fi
+
 cd api
 CGO_ENABLED=0 go build
 if [ $? -ne 0 ]; then
@@ -30,9 +38,6 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 cd ..
-
-chmod 777 docker/pg-admin/
-chmod 777 docker/pg-admin/servers.json
 
 sudo docker-compose up -d --force-recreate --build --remove-orphans
 
